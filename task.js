bb4859ca266ae1cb7d1541c6e2409afbc7199413
HaydenChan8619 <haydenphchan@gmail.com>
2023-09-10 11:49:02 -0700

    Implemented saving functionality, removed time functionality
    Due to time constraints and workload, I decided to shelf the timer functionality for now.

--- task.js
+++ task.js
@@ -3,34 +3,63 @@ const task = document.getElementById('task');
 const confrimTask = document.getElementById('confirmTask');
 const taskList = document.getElementById('taskList');
 
-let taskMap = new Map();
+let taskListItems = [];
+
+function loadTasksFromStorage() {
+    chrome.storage.sync.get('tasks', function(data) {
+        if (data.tasks) {
+            taskListItems = data.tasks;
+            updateTaskList();
+        }
+    });
+}
+
+function saveTasksToStorage() {
+    chrome.storage.sync.set({ 'tasks': taskListItems });
+}
+
+// Load tasks when open/reopen extension
+loadTasksFromStorage();
 
 confrimTask.addEventListener('click', () => {
     console.log('Button clicked');
     const taskValue = task.value;
 
-    if (taskValue.trim() !== '' && !(taskMap.has(taskValue))) {
-        taskMap.set(taskValue,0); // push the task in with a time of 0 seconds
+    if (taskValue.trim() !== '') {
+        taskListItems.push(taskValue);
         task.value = '';
         updateTaskList();
-    }
 
-})
+        // Save tasks to Chrome storage when a new task is added
+        saveTasksToStorage();
+    }
+});
 
 function updateTaskList() {
     taskList.innerHTML = '';
 
-    for (const[taskName, taskTime] of taskMap) {
+    for (const taskName of taskListItems) {
         const listHtml = document.createElement('li');
         const taskDiv = document.createElement('div');
         const taskNameSpan = document.createElement('span');
-        const taskTimeSpan = document.createElement('span');
-        
+
         taskNameSpan.textContent = taskName;
-        taskTimeSpan.textContent = `Time Spent: ${taskTime} seconds`;
+
+        const completeButton = document.createElement('button');
+        completeButton.textContent = 'Complete Task';
+        completeButton.addEventListener('click', () => {
+            // Implement the delete task functionality here
+            const taskIndex = taskListItems.indexOf(taskName);
+            if (taskIndex !== -1) {
+                taskListItems.splice(taskIndex, 1);
+                updateTaskList();
+                // Save tasks to Chrome storage when a task is completed
+                saveTasksToStorage();
+            }
+        });
 
         taskDiv.appendChild(taskNameSpan);
-        taskDiv.appendChild(taskTimeSpan);
+        taskDiv.appendChild(completeButton);
 
         listHtml.appendChild(taskDiv);
 
